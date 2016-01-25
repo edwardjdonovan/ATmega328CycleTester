d0a367969ded3e4daa2c013b3c3f7f2ee068ffa9
Edward Donovan <ejoseph.donovan@gmail.com>
2016-01-25 15:59:16 -0500

    release v0.1.0 build

---
+++ ED5000_CT_Atmel_Studios/ED5000_CT_ATmega328P_app/ED5000_CT_ATmega328P_app/Release/makedep.mk
@@ -0,0 +1,10 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+src\ASF\common\services\clock\mega\sysclk.c
+
+src\ASF\mega\boards\stk600\rcx_x\init.c
+
+src\main.c
+
